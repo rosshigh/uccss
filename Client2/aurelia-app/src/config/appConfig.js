a71dd45dcc87c899824e40a456f2911f702ce082
Ross Hightower <hightowe@uwm.edu>
2016-12-02 12:33:06 -0600

    fix url

--- Client2/aurelia-app/src/config/appConfig.js
+++ Client2/aurelia-app/src/config/appConfig.js
@@ -4,9 +4,9 @@ import {HttpClient} from 'aurelia-http-client';
 @inject(HttpClient)
 export class AppConfig {
 
-    BASE_URL = "http://localhost:5000/api/";; 
     HOST = location.origin;
-    // BASE_URL = this.HOST + "/api/";
+    // BASE_URL = "http://localhost:5000/api/";; 
+    BASE_URL = this.HOST + "/api/";
     HELPTICKET_FILE_DOWNLOAD_URL = this.HOST + "/uploadedFiles/helpTickets";
     PRODUCT_FILE_DOWNLOAD_URL = this.HOST + "/uploadedFiles/productFiles";
     DOWNLOAD_FILE_DOWNLOAD_URL = this.HOST + '/uploadedFiles/downloads';
