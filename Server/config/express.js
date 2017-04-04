5f32a0c4a0fa9a543c0216dc18dc00cd611dbc59
Ross Hightower <hightowe@uwm.edu>
2017-04-04 13:03:37 -0500

    larger file size

--- Server/config/express.js
+++ Server/config/express.js
@@ -32,8 +32,8 @@ module.exports = function(app, config) {
   });
 
   logger.log("Attaching plugins");
-  app.use(bodyParser.json({limit: '50mb'}));
-  app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+  app.use(bodyParser.json({limit: '1000mb'}));
+  app.use(bodyParser.urlencoded({limit: '1000mb', extended: true}));
   // app.use(bodyParser.urlencoded({extended: true}));
   // app.use(bodyParser.json());
   app.use(require('compression')());
