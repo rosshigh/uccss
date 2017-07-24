1c62dbf17e1df032eefc96e5c565bfb8780acf4b
Ross Hightower <hightowe@uwm.edu>
2017-07-24 12:34:19 -0500

    assignment bug

--- Client/scripts/app-bundle.js
+++ Client/scripts/app-bundle.js
@@ -24465,7 +24465,8 @@ define('modules/tech/requests/assignments',['exports', 'aurelia-framework', 'aur
             this.requestSelected = 'form';
 
             this.editIndex = this.dataTable.getOriginalIndex(index);
-            this.requests.selectRequestDetailFromId(request._id);
+
+            this.products.selectedProductFromId('5964d133503dd106746c1309');
         };
 
         Assignments.prototype.clientRequired = function clientRequired() {
--- Client/src/modules/tech/requests/assignments.js
+++ Client/src/modules/tech/requests/assignments.js
@@ -131,10 +131,10 @@ export class Assignments {
 
         // //Retrieve relevant data
         this.editIndex = this.dataTable.getOriginalIndex(index);
-        this.requests.selectRequestDetailFromId(request._id);
+        // this.requests.selectRequestDetailFromId(request._id);
         // this.requests.selectRequestDetail(this.editIndex);
         // this.products.selectedProductFromId(this.requests.selectedRequestDetail.productId);
-        // this.products.selectedProductFromId('5964d133503dd106746c1309');
+        this.products.selectedProductFromId('5964d133503dd106746c1309');
 
         // this.provisionalAssignment = this.requests.selectedRequestDetail.requestStatus == this.config.PROVISIONAL_REQUEST_CODE;
         
