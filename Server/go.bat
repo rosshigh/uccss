a4f2245db3cdb1d6e52c9f3f63b7905c772d0107
Ross Hightower <hightowe@uwm.edu>
2017-08-05 12:27:20 -0500

    go batch file

--- Server/go.bat
+++ Server/go.bat
@@ -1,1 +1,2 @@
-forever start -l ./forever-log/forever.log -o ./forever-log/out.log -e ./forever-log/err.log index.js
+%NODE_ENV% = Production
+forever start -l c:\uccss\forever-log\forever.log -o .\forever-log\out.log -e .\forever-log\error.log -w index.js
