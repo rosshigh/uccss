11fba197842ad032f84a2644684c7cdb346eb236
Ross Hightower <hightowe@uwm.edu>
2017-08-30 11:08:07 -0500

    multiselect

--- Client/src/resources/index.js
+++ Client/src/resources/index.js
@@ -2,7 +2,7 @@ export function configure(config) {
   config.globalResources([
     './editor/editor',
     './elements/calendar',
-    './elements/multiselect',
+    // './elements/multiselect',
     './elements/tree-node',
     './elements/submenu',
     './elements/nav-bar',
