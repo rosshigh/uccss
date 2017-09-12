37c9f312b2ebf5d08b0e63d87adb234c25c54c65
Ross Hightower <hightowe@uwm.edu>
2017-09-11 22:27:40 -0500

    bulk email

--- Server/app/controllers/people.js
+++ Server/app/controllers/people.js
@@ -44,6 +44,7 @@ module.exports = function (app, config) {
   router.get('/api/people/bulkEmail', function(req, res, next){
     logger.log('Get people builkEmail', 'verbose');
     var query = buildQuery(req.query, Model.find());
+    query.select('lastName firstName institutionId fullName personStatus email roles')
     query.populate('institutionId',{institutionType: 1, memberType: 1, name: 1, region: 1, city: 1, country: 1})
     query.exec( function(err, object){
         if (err) {
