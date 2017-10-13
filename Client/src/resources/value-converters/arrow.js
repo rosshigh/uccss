928ab05be9a143813c3bceb263d68d7a94872373
Ross Hightower <hightowe@uwm.edu>
2017-10-13 12:22:45 -0500

    arrow

--- Client/src/resources/value-converters/arrow.js
+++ Client/src/resources/value-converters/arrow.js
@@ -1,6 +1,6 @@
 export class ArrowValueConverter {
-	toView(value1, value2) {
-	  if(value1 == value2){
+	toView(value1, value2, value3, value4) {
+	  if(value1 == value2 && value3 == value4){
 		  return '<i class="fa fa-arrow-left" aria-hidden="true"></i>';
 	  } else {
 		  return '';
