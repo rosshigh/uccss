587c9282218d595d503dbf0236f7b2bdafde0628
Ross Hightower <hightowe@uwm.edu>
2019-01-28 13:12:14 -0600

    changes

--- Client/src/modules/tech/support/viewHelpTickets.js
+++ Client/src/modules/tech/support/viewHelpTickets.js
@@ -14,6 +14,7 @@ import { CommonDialogs } from '../../../resources/dialogs/common-dialogs';
 import { ClientRequests } from '../../../resources/data/clientRequests';
 import { DocumentsServices } from '../../../resources/data/documents';
 
+
 @inject(Router, AppConfig, Validation, People, CommonDialogs, DataTable, Utils, HelpTickets, Sessions, Systems, Downloads, Products, TemplatingEngine, ClientRequests, DocumentsServices, ClientRequests)
 export class ViewHelpTickets {
   helpTicketSelected = false;
